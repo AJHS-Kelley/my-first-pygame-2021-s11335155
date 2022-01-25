12a7320fe73e9a584bf29c13ea7bc5ca45a9b3fc
Linda Cooper <s11335155@students.duvalschools.org>
2022-01-25 14:10:59 -0500

    v1.0
    Wrote code to move the player.
    
    Draw player to window surface.

--- collision_detection.py
+++ collision_detection.py
@@ -1,4 +1,4 @@
-# PyGame Collision Detection Practice, Linda Cooper, January 11 2022, 2:05pm, v0.9
+# PyGame Collision Detection Practice, Linda Cooper, January 11 2022, 2:10pm, v1.0
 
 import pygame, sys, random
 from pygame,locals import *
@@ -85,3 +85,16 @@ if foundCounter => NEWFOOD:
 
 # Draw white background on Window Surface.
 windowSurface.fill(WHITE)
+
+# Move the player.
+if moveDown and player.bottom < WINDOWHEIGHT:
+    player.top += MOVESPEED
+if moveUp and player.top > 0:
+    player.top -= MOVESPEED
+if moveLeft and player.left > 0:
+        player.left -= MOVESPEED
+if moveRight and player.right < WINDOWWIDTH:
+    player.right += MOVESPEED
+
+# Draw the player on the surface.
+pygame.draw.rect(windowSurface, BLACK, player)
